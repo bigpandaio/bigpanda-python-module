d9548ad3e958871a1aaf3e82f507fc09316e2557
Hagai <hagai@bigpanda.io>
2014-05-04 19:07:06 +0300

    Add setup.py to python module

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='bigpanda',
+      version='1.5.0',
+      description='Python module for integration with BigPanda',
+      author='BigPanda',
+      author_email='support@bigpanda.io',
+      url='bigpanda.io',
+      packages=['bigpanda']
+)
