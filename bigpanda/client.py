15481bc4bc2fdd78e016cc8690b79ca1579578c4
Hagai <hagai@bigpanda.io>
2014-05-04 19:02:43 +0300

    Add bigpanda python module

---
+++ bigpanda/client.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+import config
+import deployment
+
+class Client(object):
+    def __init__(self, api_token, base_url=config.base_url):
+        self.api_token = api_token
+        self.base_url = base_url
+
+    def deployment(self, component, version, hosts, owner=None, source=None, env=None):
+        """
+        Return a new Deployment object. Refer to bigpanda.Deployment for more help.
+        """
+        return deployment.Deployment(self, component, version, hosts, owner, source, env)
+
+    def api_call(self, endpoint, data=None):
+        headers = {'Authorization': 'Bearer %s' % self.api_token,
+                    'Content-Type': 'application/json'}
+
+        if data:
+            self.data = data
+            r = requests.post(self.base_url + endpoint, data=json.dumps(data), headers=headers)
+        else:
+            r = requests.get(self.base_url + endpoint, headers=headers)
+
+        r.raise_for_status()
---
+++ bigpanda/deployment.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+
+class Deployment(object):
+    """
+    BigPanda deployment object.
+
+    methods:
+    start():            Mark deployment as started
+    success():          Mark deployment as successful
+    failure(message):   Mark deployment as failed with optional error message
+    """
+
+    deployment_start_endpoint = '/data/events/deployments/start'
+    deployment_end_endpoint = '/data/events/deployments/end'
+
+    def __init__(self, client, component, version, hosts, owner=None, source=None, env=None):
+        """
+        Create a new deployment.
+
+        client:     Client object. Client.deployment() passes this object for you.
+        component:  Name of the application being deployed
+        version:    Version of the application being deployed
+        hosts:      Name or list of names of the hosts being deployed on
+        owner:      Optional name of the person responsible
+        source:     Optional name of the system triggering this deployment (ex: Fabric)
+        env:        Optional name of the environment being deployed on (ex: stage)
+        """
+        if not isinstance(hosts, list):
+            hosts = [ hosts ]
+
+        self._client = client
+        self.component = component
+        self.version = version
+        self.hosts = hosts
+        self.owner = owner
+        self.source = source
+        self.env = env
+
+    def start(self):
+        """
+        Notify BigPanda on the start of this deployment
+        """
+        tmp_call_data = dict( component=self.component,
+                          version=self.version,
+                          hosts=self.hosts,
+                          owner=self.owner,
+                          source=self.source,
+                          env=self.env)
+        call_data = dict()
+        for k in tmp_call_data:
+            if tmp_call_data[k] is not None:
+                call_data[k] = tmp_call_data[k]
+
+        self._client.api_call(self.deployment_start_endpoint, data=call_data)
+
+        return self
+
+    def success(self):
+        """
+        Notify BigPanda this deployment has succeeded
+        """
+        call_data = dict( component=self.component,
+                          version=self.version,
+                          hosts=self.hosts,
+                          status='success')
+
+        self._client.api_call(self.deployment_end_endpoint, call_data)
+
+        return self
+
+    def failure(self, message=None):
+        """
+        Notify BigPanda this deployment has succeeded.
+
+        message is an optional error message.
+        """
+        call_data = dict( component=self.component,
+                          version=self.version,
+                          hosts=self.hosts,
+                          status='failure')
+        if message:
+            call_data['errorMessage'] = message
+
+        self._client.api_call(self.deployment_end_endpoint, call_data)
+
+        return self
