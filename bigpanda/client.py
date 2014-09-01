cd8a4ebc3c83c625e085739e37c88cb030442588
Hagai Kariti <hagai@bigpanda.io>
2014-09-01 16:06:21 +0300

    Add send() method to client

--- bigpanda/client.py
+++ bigpanda/client.py
@@ -6,8 +6,9 @@ import deployment
 import alert
 
 class Client(object):
-    def __init__(self, api_token, base_url=config.base_url):
+    def __init__(self, api_token, app_key=None, base_url=config.base_url):
         self.api_token = api_token
+        selff.app_key = app_key
         self.base_url = base_url
 
     def deployment(self, component, version, hosts, owner=None, env=None):
@@ -22,7 +23,38 @@ class Client(object):
         """
         return alert.Alert(self, app_key, subject, check, description, cluster, timestamp, primary_attr, secondary_attr, **kwargs)
 
-    def api_call(self, endpoint, data=None):
+    def send(self, data):
+        """
+        Send an alert or deployment object.
+        
+        Normally equivalent to calling .send() on the object itself, but accepts a list
+        of alerts/deployment to send in a single api call.
+        """
+
+        data_type = self._get_data_type(data)
+
+        if isinstance(data, list):
+            if data_type != 'alert':
+                raise TypeError('Batch mode is only supported for alerts.')
+
+            messages = list()
+            for m in data:
+                messages.append(m._build_payload)
+            payload = dict(alerts=messages)
+            endpoint = data[0]._endpoint
+        else:
+            payload = data._build_payload()
+            endpoint = data._endpoint
+
+        # Deployments don't have app_key just yet
+        if data_type == 'alert':
+            if not self.app_key:
+                raise RuntimeError("app_key is not set")
+            payload['app_key'] = self.app_key
+
+        self._api_call(endpoint, payload)
+
+    def _api_call(self, endpoint, data=None):
         headers = {'Authorization': 'Bearer %s' % self.api_token,
                     'Content-Type': 'application/json'}
 
